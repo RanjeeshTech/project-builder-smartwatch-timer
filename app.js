821fc431ed43dae6411ed3ddef051b0654dbcef8
RanjeeshTech <ranjeeshr07012002@gmail.com>
2021-12-05 20:29:02 +0530

    done

--- app.js
+++ app.js
@@ -56,6 +56,19 @@ function openMessage(e) {
 const main = document.querySelector(".main-section");
 main.addEventListener("click", openMessage);
 
+let minutes = 0,
+    seconds = 0,
+    hours = 0,
+    days = 0,
+    months = 0,
+    years = 0,
+    sec = 0,
+    min = 0,
+    day = 0,
+    hour = 0,
+    month = 0,
+    year = 0;
+
 function showIconSection() {
     clearInterval(interval);
     icons.forEach(icon => {
@@ -63,17 +76,47 @@ function showIconSection() {
             if (this.classList.contains("iconActive")) {
                 icon.classList.remove("iconActive");
                 if (this.dataset.name === "clock") {
+                    if (seconds < 10) {
+                        sec = '0' + seconds;
+                    } else {
+                        sec = seconds;
+                    }
+                    if (minutes < 10) {
+                        min = '0' + minutes;
+                    } else {
+                        min = minutes;
+                    }
+                    if (hours < 10) {
+                        hour = '0' + hours;
+                    } else {
+                        hour = hours;
+                    }
+                    if (days < 10) {
+                        day = '0' + days;
+                    } else {
+                        day = days;
+                    }
+                    if (months < 10) {
+                        month = '0' + months;
+                    } else {
+                        month = months;
+                    }
+                    if (years < 10) {
+                        year = '0' + years;
+                    } else {
+                        year = years;
+                    }
                     main.innerHTML = `
                             <div class="inner-main">
                                 <div class="main-stopTime">
-                                    <p>00:00:22</p>
+                                    <p><span class="years">${year}</span>:<span class="months">${month}</span>:<span class="days">${day}</span></p>
                                 </div>
                                 <div class="sub-stopTime">
-                                    <p>00:00:15</p>
+                                    <p><span class="hours">${hour}</span>:<span class="minutes">${min}</span>:<span class="seconds">${sec}</span></p>
                                 </div>
                                 <div class="stop-icons">
                                     <i class="fas fa-redo" data-stop="redo"></i>
-                                    <i class="fas fa-pause" data-stop="pause"></i>
+                                    <div class="startStop"><i class="fas fa-play" data-stop="start"></i></div>
                                     <i class="fas fa-stop" data-stop="stop"></i>
                                 </div>
                             </div>                 
@@ -99,3 +142,183 @@ function showIconSection() {
 
 
 icons.forEach(icon => icon.addEventListener("click", showIconSection));
+
+function increaseTime() {
+    seconds++;
+    if (seconds >= 59) {
+        minutes++;
+    }
+    if (minutes >= 59) {
+        hours++;
+    }
+    if (hours >= 23) {
+        days++;
+    }
+    if (days >= 30) {
+        months++;
+    }
+    if (months >= 11) {
+        years++;
+    }
+    console.log(seconds);
+    if (document.querySelector(".seconds") != null) {
+        if (seconds < 10) {
+            document.querySelector(".seconds").innerHTML = '0' + seconds;
+        } else {
+            document.querySelector(".seconds").innerHTML = seconds;
+        }
+    }
+    if (document.querySelector(".minutes") != null && seconds == 59) {
+        if (minutes < 10) {
+            document.querySelector(".minutes").innerHTML = '0' + minutes;
+        } else {
+            document.querySelector(".minutes").innerHTML = minutes;
+        }
+        seconds = 0;
+    }
+    if (document.querySelector(".hours") != null && minutes == 59) {
+        if (hours < 10) {
+            document.querySelector(".hours").innerHTML = '0' + hours;
+        } else {
+            document.querySelector(".hours").innerHTML = hours;
+        }
+        minutes = 0;
+    }
+    if (document.querySelector(".days") != null && hours == 234) {
+        if (days < 10) {
+            document.querySelector(".days").innerHTML = '0' + days;
+        } else {
+            document.querySelector(".days").innerHTML = days;
+        }
+        hours = 0;
+    }
+    if (document.querySelector(".months") != null && days == 30) {
+        if (months < 10) {
+            document.querySelector(".months").innerHTML = '0' + months;
+        } else {
+            document.querySelector(".months").innerHTML = months;
+        }
+        days = 0;
+    }
+    if (document.querySelector(".years") != null && months == 11) {
+        if (years < 10) {
+            document.querySelector(".years").innerHTML = '0' + years;
+        } else {
+            document.querySelector(".years").innerHTML = years;
+        }
+        months = 0;
+    }
+
+}
+
+
+var inter;
+const stopPausePlay = (e) => {
+    console.log(e.target.dataset.stop);
+    if (e.target.dataset.stop == "start") {
+        document.querySelector(".startStop").innerHTML = `<i class="fas fa-pause" data-stop="pause"></i>`;
+        if (inter) {
+            clearInterval(inter);
+        }
+        if (seconds == 0 && minutes == 0 && hours == 0 && months == 0 && days == 0 && years == 0) {
+            document.querySelector(".seconds").innerHTML = '00';
+            document.querySelector(".minutes").innerHTML = '00';
+            document.querySelector(".hours").innerHTML = '00';
+            document.querySelector(".days").innerHTML = '00';
+            document.querySelector(".months").innerHTML = '00';
+            document.querySelector(".years").innerHTML = '00';
+        }
+        inter = setInterval(increaseTime, 1000);
+
+    } else if (e.target.dataset.stop == "redo") {
+        clearInterval(inter);
+        document.querySelector(".startStop").innerHTML = `<i class="fas fa-play" data-stop="start"></i>`;
+        document.querySelector(".seconds").innerHTML = '00';
+        document.querySelector(".minutes").innerHTML = '00';
+        document.querySelector(".hours").innerHTML = '00';
+        document.querySelector(".days").innerHTML = '00';
+        document.querySelector(".months").innerHTML = '00';
+        document.querySelector(".years").innerHTML = '00';
+        seconds = 0;
+        minutes = 0;
+        hours = 0;
+        days = 0;
+        months = 0;
+        years = 0;
+    } else if (e.target.dataset.stop == "pause") {
+        document.querySelector(".startStop").innerHTML = `<i class="fas fa-play" data-stop="start"></i>`;
+        clearInterval(inter);
+        if (seconds < 10) {
+            document.querySelector(".seconds").innerHTML = '0' + seconds;
+        } else {
+            document.querySelector(".seconds").innerHTML = seconds;
+        }
+        if (minutes < 10) {
+            document.querySelector(".minutes").innerHTML = '0' + minutes;
+        } else {
+            document.querySelector(".minutes").innerHTML = minutes;
+        }
+        if (hours < 10) {
+            document.querySelector(".hours").innerHTML = '0' + hours;
+        } else {
+            document.querySelector(".hours").innerHTML = hours;
+        }
+        if (days < 10) {
+            document.querySelector(".days").innerHTML = '0' + days;
+        } else {
+            document.querySelector(".days").innerHTML = days;
+        }
+        if (months < 10) {
+            document.querySelector(".months").innerHTML = '0' + months;
+        } else {
+            document.querySelector(".months").innerHTML = months;
+        }
+        if (years < 10) {
+            document.querySelector(".years").innerHTML = '0' + years;
+        } else {
+            document.querySelector(".years").innerHTML = years;
+        }
+    } else if (e.target.dataset.stop == "stop") {
+        document.querySelector(".startStop").innerHTML = `<i class="fas fa-play" data-stop="start"></i>`;
+        clearInterval(inter);
+        if (seconds < 10) {
+            document.querySelector(".seconds").innerHTML = '0' + seconds;
+        } else {
+            document.querySelector(".seconds").innerHTML = seconds;
+        }
+        if (minutes < 10) {
+            document.querySelector(".minutes").innerHTML = '0' + minutes;
+        } else {
+            document.querySelector(".minutes").innerHTML = minutes;
+        }
+        if (hours < 10) {
+            document.querySelector(".hours").innerHTML = '0' + hours;
+        } else {
+            document.querySelector(".hours").innerHTML = hours;
+        }
+        if (days < 10) {
+            document.querySelector(".days").innerHTML = '0' + days;
+        } else {
+            document.querySelector(".days").innerHTML = days;
+        }
+        if (months < 10) {
+            document.querySelector(".months").innerHTML = '0' + months;
+        } else {
+            document.querySelector(".months").innerHTML = months;
+        }
+        if (years < 10) {
+            document.querySelector(".years").innerHTML = '0' + years;
+        } else {
+            document.querySelector(".years").innerHTML = years;
+        }
+        seconds = 0;
+        minutes = 0;
+        hours = 0;
+        days = 0;
+        months = 0;
+        years = 0;
+    }
+}
+
+const stopWatchIcons = document.querySelector(".stop-icons");
+main.addEventListener("click", stopPausePlay);
